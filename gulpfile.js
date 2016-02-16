42a709a07b84d8d546f0db1b5daaf8d47c7b41eb
Greird <h.fauconnier@gmail.com>
2016-02-16 22:10:53 +0100

    Adding Gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Load plugins
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('src/*.js')
+        .pipe(gulp.dest('build'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('build'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Default task
+gulp.task('default', ['lint', 'scripts', 'watch']);
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .js files
+  gulp.watch('src/*.js', ['lint', 'scripts']);
+
+});
