2f90247d721a0c532b488ab4c021620e9fda1cdc
greird <h.fauconnier@gmail.com>
2016-07-11 22:05:53 +0200

    Added m6/m7b5 chords to unit test.. raised an issue :(

--- _testfiles/tests.js
+++ _testfiles/tests.js
@@ -34,7 +34,10 @@ QUnit.test("guitar.getChordInfo()", function(assert) {
 		// 7
 		['131211', 'F7', 'FCD#ACF', '1-3-5-b7'],
 		// Minor 7 or 6
-		['133231', ["F6", "Dm7"], 'FCFADF', ["1-3-5-6", "1-b3-5-b7"]]
+		['133231', ["F6", "Dm7"], 'FCFADF', ["1-3-5-6", "1-b3-5-b7"]], 
+		// Minor 6 or Minor 7b5
+		['xx5545', ["Cm6", "Am7b5"], 'xxGCD#A', ["1-b3-5-6", "1-b3-b5-b7"]],
+		['8988xx', ["D#m6", "Cm7b5"], 'CF#A#D#xx', ["1-b3-5-6", "1-b3-b5-b7"]]
 	];
 
 	for (var i = 0; i < chords.length; i++) {
@@ -43,20 +46,20 @@ QUnit.test("guitar.getChordInfo()", function(assert) {
 		if (chords[i][1].constructor === Array) {
 			assert.deepEqual(guitar.getChordInfo(chords[i][0]).name, chords[i][1], chords[i][0] + " is an " + chords[i][1] + " chord");
 		} else {
-			assert.ok(guitar.getChordInfo(chords[i][0]).name == chords[i][1], chords[i][0] + " is an " + chords[i][1] + " chord");
+			assert.equal(guitar.getChordInfo(chords[i][0]).name, chords[i][1], chords[i][0] + " is an " + chords[i][1] + " chord");
 		}
 
 		// Check chord formula(s)
 		if (chords[i][3].constructor === Array) {
 			assert.deepEqual(guitar.getChordInfo(chords[i][0]).formula, chords[i][3], chords[i][0] + " formula is " + chords[i][3]);
 		} else {
-			assert.ok(guitar.getChordInfo(chords[i][0]).formula == chords[i][3], chords[i][0] + " formula is " + chords[i][3]);
+			assert.equal(guitar.getChordInfo(chords[i][0]).formula, chords[i][3], chords[i][0] + " formula is " + chords[i][3]);
 		}
 
 		// Check chord notes
-		assert.ok(guitar.getChordInfo(chords[i][0]).notes == chords[i][2], chords[i][0] + " contains the following notes " + chords[i][2]);
+		assert.equal(guitar.getChordInfo(chords[i][0]).notes, chords[i][2], chords[i][0] + " contains the following notes " + chords[i][2]);
 
 		// Check chord tab
-		assert.ok(guitar.getChordInfo(chords[i][0]).tab == chords[i][0], chords[i][0] + " tab is " + chords[i][0]);
+		assert.equal(guitar.getChordInfo(chords[i][0]).tab, chords[i][0], chords[i][0] + " tab is " + chords[i][0]);
 	}
 });
