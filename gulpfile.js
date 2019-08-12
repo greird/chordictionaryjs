4d6e27ee6847640c1090df8fa1e6e41fcc301e1e
Hubert Fauconnier <h.fauconnier@gmail.com>
2019-08-12 17:03:43 +0200

    build and minify lib in 3 versions: es6, CommonJS and IIFE

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,8 @@ csso = require('gulp-csso'),
 rename = require('gulp-rename'),
 qunit = require('gulp-qunit'),
 babel = require("gulp-babel");
-minify = require("gulp-babel-minify");
-
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-	return gulp.src('./src/chordictionary.js')
-	.pipe(babel())
-	.pipe(rename({ suffix: '.min' }))
-	.pipe(minify())
-	.pipe(gulp.dest('./build'))
-});
+minify_babel = require("gulp-babel-minify");
+minify = require('gulp-minify');
 
 // Lint Task
 gulp.task('lint', function() {
@@ -22,6 +14,37 @@ gulp.task('lint', function() {
 	.pipe(eslint())
 	.pipe(eslint.format())
 });
+
+//  Transpile for ES6, commonJS and IIFE compatibility, then minify JS
+gulp.task('build_es6', function() {
+	return gulp.src('./src/chordictionary.js')
+	.pipe(minify({
+		ext: {
+			src:'.js',
+			min:'_es6.min.js'
+			}, 
+		noSource: true
+		}))
+	.pipe(gulp.dest('./build'))
+});
+gulp.task('build_commonJS', function() {
+	return gulp.src('./src/chordictionary.js')
+	.pipe(babel())
+	.pipe(rename('chordictionary_commonjs.min.js'))
+	.pipe(minify_babel())
+	.pipe(gulp.dest('./build'))
+});
+gulp.task('build_iife', function() {
+	/* NOTE: need to build IIFE file first with Rolloup 
+		> npm install --global rollup
+		> rollup src/chordictionary.js --file src/chordictionary_iife.js --format iife --name chordictionary
+		*/
+		return gulp.src('./src/chordictionary_iife.js')
+		.pipe(babel())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(minify_babel())
+		.pipe(gulp.dest('./build'))
+	});
 
 // CSS task
 gulp.task('css', function () {
@@ -38,17 +61,17 @@ gulp.task('test', function() {
 });
 
 // Default task
-gulp.task('default', ['watch'], function() {
-	gulp.start('lint', 'scripts', 'css');
+gulp.task('default', function() {
+	gulp.start('lint', 'build_es6', 'build_commonJS', 'build_iife', 'css');
 });
 
  // Build task
  gulp.task('build', function() {
- 	gulp.start('css', 'lint', 'scripts', 'test');
+ 	gulp.start('css', 'lint', 'build_es6', 'build_commonJS', 'build_iife', 'test');
  });
 
 // Watch
 gulp.task('watch', function() {
-	gulp.watch('./src/*.js', ['lint', 'scripts']);
+	gulp.watch('./src/*.js', ['lint', 'build_es6', 'build_commonJS', 'build_iife']);
 	gulp.watch('./src/chordictionary.css', ['css']);
 });
